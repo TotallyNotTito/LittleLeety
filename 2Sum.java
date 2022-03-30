3a58742505201d590e947c5723e0ceaedd1bdf62
Tito Reinhart <59128315+TotallyNotTito@users.noreply.github.com>
2022-03-30 07:13:21 -0700

    Finding 2Sum from list
    hash Map Linear solution, and commented brute force solution

--- 2Sum.java
+++ 2Sum.java
@@ -1,3 +1,17 @@
+public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[] { map.get(complement), i };
+        }
+        map.put(nums[i], i);
+    }
+        // In case there is no solution, we'll just return null
+    return null;
+}
+
+/*
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int [] indices = new int[2];
@@ -14,3 +28,4 @@ class Solution {
         
     }
 }
+*/
