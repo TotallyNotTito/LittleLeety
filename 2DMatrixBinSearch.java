4945c7135b917c93a54fc88491403468f6f088a8
Tito Reinhart <59128315+TotallyNotTito@users.noreply.github.com>
2022-03-30 06:59:02 -0700

    Update 2DMatrixBinSearch.java

--- 2DMatrixBinSearch.java
+++ 2DMatrixBinSearch.java
@@ -1,24 +1,22 @@
-class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        if (m == 0)
-            return false;
-        int n = matrix[0].length;
-        //bin search
-        int left = 0, right = m * n - 1;
-        int pivotIndex, pivotElement;
-        while (left <= right) {
-            pivotIndex = (left + left) / 2;
-            pivotElement = matrix[pivotIndex / n][pivotIndex % n];
-            if (target == pivotElement)
-                return true;
-            else {
-                if (target < pivotElement) 
-                    right = pivotIndex - 1;
-                else 
-                    left = pivotIndex + 1;
-            }
-        }
+public boolean searchMatrix(int[][] matrix, int target) {
+    int m = matrix.length;
+    if (m == 0)
         return false;
+    int n = matrix[0].length;
+    //bin search
+    int left = 0, right = m * n - 1;
+    int pivotIndex, pivotElement;
+    while (left <= right) {
+        pivotIndex = (left + right) / 2;
+        pivotElement = matrix[pivotIndex / n][pivotIndex % n];
+        if (target == pivotElement)
+            return true;
+        else {
+            if (target < pivotElement) 
+                right = pivotIndex - 1;
+            else 
+                left = pivotIndex + 1;
+        }
     }
+    return false;
 }
