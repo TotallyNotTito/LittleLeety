ecd0f9b9ec47b19c4fd469ac43d1e963aa89df32
Tito Reinhart <59128315+TotallyNotTito@users.noreply.github.com>
2022-03-31 11:17:19 -0700

    Update lengthLongestSubString.java

--- lengthLongestSubString.java
+++ lengthLongestSubString.java
@@ -1,4 +1,26 @@
+public int lengthOfLongestSubstring(String s) {
+    Integer [] chars = new Integer[128]; //integer table
+    int left = 0; //left sliding window
+    int right = 0; //right sliding window
+        
+    int result = 0; //value of size of largest substring
+    while (right < s.length()) {
+        char r = s.charAt(right); //index of right window
+            
+        Integer index = chars[r];
+        //index has been visited or repeated character, move left to close up window and not repeat visiting duplicate characters
+        if (index != null && index >= left && index < right) left = index + 1;
+            
+        result = Math.max(result, right - left + 1);
+            
+        chars[r] = right;
+        right++;
+    }
+        
+    return result;
+}
 
+/*
 public int lengthOfLongestSubstring(String s) {
     int [] chars = new int[128];
         
@@ -22,7 +44,7 @@ public int lengthOfLongestSubstring(String s) {
     }
     return result;
 }
-/* with hash map
+with hash map
 public int lengthOfLongestSubstring(String s) {
     int n = s.length(), result = 0;
     Map<Character, Integer> map = new HashMap<>(); //index of current character
