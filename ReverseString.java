09c20a766d8e837223ad1004eea384224adbef25
Tito Reinhart <59128315+TotallyNotTito@users.noreply.github.com>
2022-03-31 12:01:53 -0700

    revese string sequence
    remove leading white space, remove multiple spaces, reverse words in list

---
+++ ReverseString.java
@@ -0,0 +1,6 @@
+public String reverseWords(String s) {
+    s = s.trim();
+    List<String> wordList = Arrays.asList(s.split("\\s+"));
+    Collections.reverse(wordList);
+    return String.join(" ", wordList);
+}
