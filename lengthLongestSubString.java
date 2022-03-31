4f7be29fbc91aa8d13d70d4beb97de5e5907ac08
Tito Reinhart <59128315+TotallyNotTito@users.noreply.github.com>
2022-03-31 11:03:59 -0700

    Update lengthLongestSubString.java

--- lengthLongestSubString.java
+++ lengthLongestSubString.java
@@ -22,3 +22,17 @@ public int lengthOfLongestSubstring(String s) {
     }
     return result;
 }
+/* with hash map
+public int lengthOfLongestSubstring(String s) {
+    int n = s.length(), result = 0;
+    Map<Character, Integer> map = new HashMap<>(); //index of current character
+    for (int i = 0, j = 0; j < n; j++) {
+        if (map.containsKey(s.charAt(j))) {
+            i = Math.max(map.get(s.charAt(j)), i);
+        }
+        result = Math.max(result, j - i + 1);
+        map.put(s.charAt(j), j + 1);
+    }
+    return result;
+}
+*/
