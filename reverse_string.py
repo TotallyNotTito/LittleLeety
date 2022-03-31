ce495453032dedd497846351585e250ed1887c1c
Tito Reinhart <59128315+TotallyNotTito@users.noreply.github.com>
2022-03-30 17:16:30 -0700

    Print strings in reverse order
    print strings in reverse order. O(N)

---
+++ reverse_string.py
@@ -0,0 +1,11 @@
+List = ['hello', 'goodbye', 'another', 'day']
+num = 0
+
+
+for i in range(len(List)) :
+    j = len(List[i]) - 1
+    while (j >= 0) :
+        print(List[i][j], end='')
+        j -= 1
+    print("\n")
+print(List)
